5f7b3c294355cd41f42f127730ecab8fb4831b32
Julian Pistorius <julian@julianpistorius.com>
2016-09-20 23:54:55 -0700

    Posting rules to a service endpoint.

--- examples/js/demo.js
+++ examples/js/demo.js
@@ -33,6 +33,22 @@ var conditions, actions, nameField, ageField, occupationField, submit, allData;
       var rule = {conditions: conditions.conditionsBuilder("data"), actions: actions.actionsBuilder("data")};
       var rules = [rule];
       console.log(JSON.stringify(rules, null, 2));
+      var rules_to_post = JSON.stringify(rules);
+      var settings = {
+        "async": true,
+        "url": "/rule",
+        "method": "POST",
+        "headers": {
+          "content-type": "application/json",
+          "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": rules_to_post
+      };
+
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
     });
   }
   $(onReady);
